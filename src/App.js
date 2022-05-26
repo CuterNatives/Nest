bafba16961308cfbd0aa76266d155a01c88d95a2
vivek26anand <vivek26anand.va@gmail.com>
2022-05-26 17:39:27 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./Dashboard";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/dashboard" exact element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
