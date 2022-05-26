5c23fe3f1f09ef033af9fe91a1f63498001355cd
vivek26anand <vivek26anand.va@gmail.com>
2022-05-26 20:12:43 +0530

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Dashboard from "./Dashboard";
+import Main from "./Main";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/dashboard" exact element={<Dashboard />} />
+        <Route path="/dashboard" exact element={<Main page="dashboard"/>} />
       </Routes>
     </Router>
   );
