74fa6793f28bebaeb44cef163af8d29e2a3793e0
Anirban Saha <anirbansaha0101@gmail.com>
2022-05-27 22:03:30 +0530

    Changed Pallete Color and Texts

--- src/Main.js
+++ src/Main.js
@@ -158,14 +158,14 @@ export default function Main(props) {
         {/* Static sidebar for desktop */}
         <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
           {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex-1 flex flex-col min-h-0 bg-gray-800">
-            <div className="flex items-center h-16 flex-shrink-0 px-4 bg-gray-900">
+          <div className="flex-1 flex flex-col min-h-0 bg-pr-800">
+            <div className="flex items-center h-16 flex-shrink-0 px-4 bg-pr-900">
               {/* <img
                 className="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
                 alt="Workflow"
               /> */}
-              <div className='text-white font-bold'>NEST by CuterNatives</div>
+              <div className='tracking-wide text-white font-bold'>N E S T</div>
             </div>
             <div className="flex-1 flex flex-col overflow-y-auto">
               <nav className="flex-1 px-2 py-4 space-y-1">
@@ -174,7 +174,7 @@ export default function Main(props) {
                     key={item.name}
                     href={item.href}
                     className={classNames(
-                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      item.current ? 'bg-gray-900 text-white' : 'text-pr-300 hover:bg-pr-700 hover:text-white',
                       'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
                     )}
                   >
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pr': {
+          '50': '#f5f5ff', 
+          '100': '#ebecff', 
+          '200': '#cdcffe', 
+          '300': '#afb2fe', 
+          '400': '#7479fd', 
+          '500': '#383ffc', 
+          '600': '#3239e3', 
+          '700': '#2a2fbd', 
+          '800': '#222697', 
+          '900': '#1b1f7b'
+      }
+
+    },
+      
+    },
   },
   plugins: [require('@tailwindcss/forms'),],
 }
