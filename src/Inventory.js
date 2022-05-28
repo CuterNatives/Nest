797f6bcb43a9c777f64853ccea1058bb85a33940
vivek26anand <vivek26anand.va@gmail.com>
2022-05-28 11:47:58 +0530

    update stock with qr

--- src/Inventory.js
+++ src/Inventory.js
@@ -135,9 +135,12 @@ export default function Example() {
                       <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{item.price}</td>
                       <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{item.stock}</td>
                       <td className="whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                        <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                        <button className="text-indigo-600 hover:text-indigo-900" onClick={()=>{
+                          setEditItem(item)
+                          setupdateopen(true)
+                        }}>
                           Update Stock
-                        </a>
+                        </button>
                       </td>
                     </tr>
                   ))}
